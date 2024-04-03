8292c873ff685b4b13c1d9ea065ebc2ac6b778eb
Will McKenzie <willmckenzie@oinutter.co.uk>
2024-04-03 13:59:58 +0100

    add more button checks

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import {axe} from "vitest-axe"
 import Button from "./Button"
 
@@ -8,6 +8,10 @@ describe("Button", () => {
     const { container } = render(<Button title="Click Me">OK</Button>)
     const results = await axe(container)
     expect(results).toHaveNoViolations()
+    const button = await screen.getByRole("button", { name: "OK"})
+    expect(button).toBeInTheDocument()
+    expect(button).toHaveAccessibleName()
+    expect(button).toHaveAttribute("title", "Click Me")
   })
 
 })
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,5 @@
 import { expect } from "vitest"
 import * as AxeMatchers from "vitest-axe/matchers"
+import "@testing-library/jest-dom"
+
 expect.extend(AxeMatchers)
